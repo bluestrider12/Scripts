9d77a33c313708b6f2ff84b18121f1a011aeb2a4
AlphaStriker <scottiekwong@gmail.com>
2017-05-28 22:12:42 -0700

    Levels

---
+++ Levels Scripts/TowerPlatform.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerPlatform : MonoBehaviour {
+
+    private static UIManager uiManager;
+    private static bool tileSelected;
+
+    public Image buidTowerUI;
+
+    // Use this for initialization
+    void Start () {
+        if(uiManager == null)
+        {
+            tileSelected = false;
+            uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void selectTile(bool active)
+    {
+        if(active)
+        {
+            tileSelected = true;
+            gameObject.GetComponent<SpriteRenderer>().color = new Color32(130, 130, 130, 255);
+        }
+        else
+        {
+            tileSelected = false;
+            gameObject.GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 255);
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        if(!tileSelected)
+        {
+            selectTile(true);
+            uiManager.enableBuildUI(gameObject);
+        }
+
+    }
+}
